7fd0791d019d33928b0df66c48645892a8598f31
Luis García <pruebasdaw2021@gmail.com>
2023-01-24 11:02:47 +0100

    Iniciada first app en Node

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Primera app con Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+module.exports = router;
